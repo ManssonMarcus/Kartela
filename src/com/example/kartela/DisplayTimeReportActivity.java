2f38327748a2d435afb0b10aaf5a8e7917c62877
Jonas Petersson <jonasbjornpetersson@gmail.com>
2014-04-10 11:41:59 +0200

    merge

--- src/com/example/kartela/DisplayTimeReportActivity.java
+++ src/com/example/kartela/DisplayTimeReportActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 public class DisplayTimeReportActivity extends ListActivity {
 	private TimelogDataSource datasource;
 	
-	@SuppressLint("NewApi")
+	@SuppressLint("NewApi")	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
