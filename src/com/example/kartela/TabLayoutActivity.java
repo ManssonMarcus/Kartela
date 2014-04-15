c9bce380ae05b7ee8fdb4283b45875d03c3ec7ee
Jessica Larsson <jessica@rebla.se>
2014-04-15 11:06:28 +0200

    added some testimages to the menu bar and changed background color

--- src/com/example/kartela/TabLayoutActivity.java
+++ src/com/example/kartela/TabLayoutActivity.java
@@ -2,6 +2,7 @@ package com.example.kartela;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
@@ -20,41 +21,47 @@ public class TabLayoutActivity extends TabActivity {
          
         // Tab for displaying time reports
         TabSpec displayspec = tabHost.newTabSpec("Display");        
-        displayspec.setIndicator("Min tid");
+        displayspec.setIndicator("");
         Intent displayIntent = new Intent(this, DisplayTimeReportActivity.class);
         displayspec.setContent(displayIntent);
         
         // Tab for reporting time
         TabSpec timespec = tabHost.newTabSpec("TimeReport");
         // setting Title and Icon for the Tab
-        timespec.setIndicator("Lägg in tid");
+        timespec.setIndicator("");
         Intent timeReportIntent = new Intent(this, TimeReportActivity.class);
         timespec.setContent(timeReportIntent);
          
         TabSpec projectspec = tabHost.newTabSpec("Project");        
-        projectspec.setIndicator("Projekt");
+        projectspec.setIndicator("");
         Intent projectIntent = new Intent(this, DisplayTimeReportActivity.class);
         projectspec.setContent(projectIntent);
         
         TabSpec reportspec = tabHost.newTabSpec("Send");        
-        reportspec.setIndicator("Skicka in");
+        reportspec.setIndicator("");
         Intent reportIntent = new Intent(this, TimeReportActivity.class);
         reportspec.setContent(reportIntent);
         
         TabSpec settingsspec = tabHost.newTabSpec("Settings");        
-        settingsspec.setIndicator("Inställningar");
+        settingsspec.setIndicator("");
         Intent settingsIntent = new Intent(this, DisplayTimeReportActivity.class);
         settingsspec.setContent(settingsIntent);
          
         // Adding all TabSpec to TabHost
         
         tabHost.addTab(displayspec); 
-        tabHost.getTabWidget().getChildAt(0).setBackgroundResource(R.drawable.tabs);
+        
+        //tabHost.getTabWidget().getChildAt(0).setBackgroundColor(Color.LTGRAY); 
+        tabHost.getTabWidget().getChildAt(0).setBackgroundResource(R.drawable.ic_action_accept);
         tabHost.addTab(timespec); 
-        tabHost.getTabWidget().getChildAt(1).setBackgroundResource(R.drawable.cloud_tab);
+        tabHost.getTabWidget().getChildAt(1).setBackgroundResource(R.drawable.ic_action_edit);
         tabHost.addTab(projectspec);
+        tabHost.getTabWidget().getChildAt(2).setBackgroundResource(R.drawable.ic_action_labels);
         tabHost.addTab(reportspec);
+        tabHost.getTabWidget().getChildAt(3).setBackgroundResource(R.drawable.ic_action_email);
         tabHost.addTab(settingsspec);
+        tabHost.getTabWidget().getChildAt(4).setBackgroundResource(R.drawable.ic_action_settings);
+        
         
         tabHost.setCurrentTab(0);
         
