9dbda2f086a4d4d34ccaaaeb1bdabf0298d5c0c6
emmwe459 <emmwe459@student.liu.se>
2014-04-28 16:38:15 +0200

    menu icons done, except for the background

--- src/com/example/kartela/TabLayoutActivity.java
+++ src/com/example/kartela/TabLayoutActivity.java
@@ -51,50 +51,6 @@ public class TabLayoutActivity extends TabActivity {
         tabHost.addTab(reportspec);
         tabHost.addTab(settingsspec);                 
 
-        /* OLD WAY         
-        // Tab for displaying time reports
-        TabSpec displayspec = tabHost.newTabSpec("Display");        
-        displayspec.setIndicator("");
-        Intent displayIntent = new Intent(this, DisplayTimeReportActivity.class);
-        displayspec.setContent(displayIntent);
-        
-        // Tab for reporting time
-        TabSpec timespec = tabHost.newTabSpec("TimeReport");
-        // setting Title and Icon for the Tab
-        timespec.setIndicator("");
-        Intent timeReportIntent = new Intent(this, TimeReportActivity.class);
-        timespec.setContent(timeReportIntent);
-         
-        TabSpec projectspec = tabHost.newTabSpec("Project");        
-        projectspec.setIndicator("");
-        Intent projectIntent = new Intent(this, ProjectListActivity.class);
-        projectspec.setContent(projectIntent);
-        
-        
-        TabSpec reportspec = tabHost.newTabSpec("Send");        
-        reportspec.setIndicator("");
-        Intent reportIntent = new Intent(this, DisplayTimeReportActivity.class);
-        reportspec.setContent(reportIntent);
-        
-        TabSpec settingsspec = tabHost.newTabSpec("Settings");        
-        settingsspec.setIndicator("");
-        Intent settingsIntent = new Intent(this, DisplaySettingsActivity.class);
-        settingsspec.setContent(settingsIntent);
-         
-        // Adding all TabSpec to TabHost
-        tabHost.addTab(displayspec); 
-        
-        //tabHost.getTabWidget().getChildAt(0).setBackgroundColor(Color.LTGRAY); 
-        tabHost.getTabWidget().getChildAt(0).setBackgroundResource(R.drawable.clock);
-        tabHost.addTab(timespec); 
-        tabHost.getTabWidget().getChildAt(1).setBackgroundResource(R.drawable.addtime);
-        tabHost.addTab(projectspec);
-        tabHost.getTabWidget().getChildAt(2).setBackgroundResource(R.drawable.projektknapp);
-        tabHost.addTab(reportspec);
-        tabHost.getTabWidget().getChildAt(3).setBackgroundResource(R.drawable.paperplane);
-        tabHost.addTab(settingsspec);
-        tabHost.getTabWidget().getChildAt(4).setBackgroundResource(R.drawable.settings);
-*/
     }
     
     
