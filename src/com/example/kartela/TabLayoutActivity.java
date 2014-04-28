719690c5d220e9b576b44a93bda4f0a9b4f6ea4f
emmwe459 <emmwe459@student.liu.se>
2014-04-28 15:16:25 +0200

    fixed menu icons

--- src/com/example/kartela/TabLayoutActivity.java
+++ src/com/example/kartela/TabLayoutActivity.java
@@ -19,9 +19,39 @@ public class TabLayoutActivity extends TabActivity {
         setContentView(R.layout.tab_layout);
          
         tabHost = getTabHost();
+        
+        TabSpec displayspec = tabHost.newTabSpec("Display");
+        displayspec.setIndicator("", getResources().getDrawable(R.drawable.icon_clock_tab));
+        Intent displayIntent = new Intent(this, DisplayTimeReportActivity.class);
+        displayspec.setContent(displayIntent);
          
+        TabSpec timespec = tabHost.newTabSpec("TimeReport");        
+        timespec.setIndicator("", getResources().getDrawable(R.drawable.icon_addtime_tab));
+        Intent timeReportIntent = new Intent(this, TimeReportActivity.class);
+        timespec.setContent(timeReportIntent);
+         
+        TabSpec projectspec = tabHost.newTabSpec("Project");
+        projectspec.setIndicator("", getResources().getDrawable(R.drawable.icon_projektknapp_tab));
+        Intent projectIntent = new Intent(this, ProjectListActivity.class);
+        projectspec.setContent(projectIntent);
+        
+        TabSpec reportspec = tabHost.newTabSpec("Send");
+        reportspec.setIndicator("", getResources().getDrawable(R.drawable.icon_paperplane_tab));
+        Intent reportIntent = new Intent(this, DisplayTimeReportActivity.class);
+        reportspec.setContent(reportIntent);
+        
+        TabSpec settingsspec = tabHost.newTabSpec("Settings");
+        settingsspec.setIndicator("", getResources().getDrawable(R.drawable.icon_settings_tab));
+        Intent settingsIntent = new Intent(this, DisplaySettingsActivity.class);
+        settingsspec.setContent(settingsIntent);
+         
+        tabHost.addTab(displayspec);
+        tabHost.addTab(timespec);
+        tabHost.addTab(projectspec);
+        tabHost.addTab(reportspec);
+        tabHost.addTab(settingsspec);                 
 
-         
+        /* OLD WAY         
         // Tab for displaying time reports
         TabSpec displayspec = tabHost.newTabSpec("Display");        
         displayspec.setIndicator("");
@@ -64,6 +94,7 @@ public class TabLayoutActivity extends TabActivity {
         tabHost.getTabWidget().getChildAt(3).setBackgroundResource(R.drawable.paperplane);
         tabHost.addTab(settingsspec);
         tabHost.getTabWidget().getChildAt(4).setBackgroundResource(R.drawable.settings);
+*/
     }
     
     
