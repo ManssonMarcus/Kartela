6d2e8fbc1e4537e331a0998ad1b23148b5eef56c
Jonas Petersson <jonasbjornpetersson@gmail.com>
2014-04-29 16:19:11 +0200

    Dynamic rectangles for total work in a week

--- src/com/example/kartela/StartscreenActivity.java
+++ src/com/example/kartela/StartscreenActivity.java
@@ -50,6 +50,7 @@ public class StartscreenActivity extends Activity implements OnClickListener{
 	private TextView tvCurrentWeek, tvTimespan;
 	private Button btnDecreaseWeek, btnIncreaseWeek;
 	private int currentWeeknumber;
+	private double total_sum = 0;
 
 	private NumberPicker weekPicker;
 	private List<String> projects;
@@ -93,12 +94,13 @@ public class StartscreenActivity extends Activity implements OnClickListener{
         
         values = datasource.getTimeInterval(currentWeeknumber);
         
-        StartscreenListAdapter adapter = new StartscreenListAdapter(this, values, weekdaysArray);
+    	// get total worked time this week
+    	for (int i = 0; i < values.size(); i++) {
+    		total_sum = total_sum + values.get(i).getWorkedTimeInNumbers();
+    	}
+        
+        StartscreenListAdapter adapter = new StartscreenListAdapter(this, values, weekdaysArray, total_sum, projects, datasource);
         weekdayList.setAdapter(adapter);
-
-        Log.d("grejs", "before upd. progressbar");
-		updateProgressBar();
-        Log.d("grejs", "Update progressbar");
 	 }
 	
 	void getWeekDays() {
@@ -176,60 +178,4 @@ public class StartscreenActivity extends Activity implements OnClickListener{
       datasource.close();
       super.onPause();
     }
-    
-    public void updateProgressBar() {
-    	// set "100 % progress" to screen width
-    	Display display = getWindowManager().getDefaultDisplay();
-    	Point size = new Point();
-    	display.getSize(size);
-
-    	int paddings = 40;
-    	int multiple = size.x - paddings;
-    	
-    	int height = 100;
-    	
-    	// temp variables
-    	double temp_sum = 0, temp_ratio = 0, total_sum = 0;
-    	TextView temp_view;
-    	String temp_name;
-    	int temp_id;
-
-    	// get total worked time this week
-    	for (int i = 0; i < values.size(); i++) {
-    		total_sum = total_sum + values.get(i).getWorkedTimeInNumbers();
-    	}
-    	Log.d("test", Double.toString(total_sum));
-    	
-    	// update progress bar for each project
-    	for(int i = 0; i < projects.size(); i++) {
-//        	Log.d("test","projects loop");
-    		temp_sum = datasource.getWorkTimeByName(projects.get(i), currentWeeknumber);
-    		temp_ratio = (temp_sum/total_sum)*100;
-    		
-    		// check ratio to round up or down
-    		if(temp_ratio - Math.floor(temp_ratio) < 0.5) {
-    			temp_ratio = Math.floor(temp_ratio);
-    		}
-    		else {
-    			temp_ratio = Math.ceil(temp_ratio);
-    		}
-    		
-    		temp_name = "progress_" + projects.get(i);
-    		temp_id = getResources().getIdentifier(temp_name, "id", getApplicationContext().getPackageName());    		
-    		temp_view = (TextView) findViewById(temp_id);
-    		
-    		// update project textview
-    		temp_view.setText(Integer.toString((int)temp_ratio) + " %");
-    		temp_view.getLayoutParams().height = height;
-    		temp_view.getLayoutParams().width = ((int)(temp_ratio*multiple/100));
-    		temp_view.requestLayout();
-    		
-    		// reset variables
-    		temp_sum = 0;
-    		temp_ratio = 0;
-    	}
-    	
-    }
-
-
 }
